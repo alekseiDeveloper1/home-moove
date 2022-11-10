76e83ca955c64bfc7d382eb00f1fb19e7c739109
Aleks <Aleksei.developer@yandex.ru>
2022-11-10 21:21:08 +0300

    add tabs js

--- src/js/script.js
+++ src/js/script.js
@@ -14,9 +14,7 @@ const updateSlider = (n) => {
   if (n < 1) {
     index = slides.length;
   }
-  slides.forEach((slide) => {
-    slide.style.display = 'none';
-  });
+  slides.forEach((slide) => slide.style.display = 'none');
   slides[index - 1].style.display = 'block';
 };
 updateSlider(index);
@@ -31,4 +29,36 @@ prev.addEventListener('click', () => {
 });
 next.addEventListener('click', () => {
   changeSlide(1);
+});
+
+// tabs
+const tabWrap = document.querySelector('.cars__tabs'),
+  tabs        = document.querySelectorAll('.cars__tab'),
+  carsBlocks  = document.querySelectorAll('.cars__block');
+
+const hideBlocks = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove('active');
+  });
+  carsBlocks.forEach((block) => {
+    block.classList.remove('active');
+  });
+};
+
+const showBlock = (i = 0) => {
+  hideBlocks();
+  tabs[i].classList.add('active');
+  carsBlocks[i].classList.add('active');
+};
+showBlock();
+
+tabWrap.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target && target.classList.contains('cars__tab')) {
+    tabs.forEach((tab, index) => {
+      if (tab === target) {
+        showBlock(index);
+      }
+    });
+  }
 });
